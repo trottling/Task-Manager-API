0653adbee379d1a2efffe8dbc31a63f7414de2a5
d.shahmatov <dmitry.shahmatov@cyberok.ru>
2025-08-13 19:51:11 +0300

    Добавлена модель задачи

--- internal/core/models/task.go
+++ internal/core/models/task.go
@@ -1,6 +1,19 @@
 package models
 
+import "time"
+
+type Status string
+
+const (
+	StatusNew        Status = "new"
+	StatusInProgress Status = "in_progress"
+	StatusDone       Status = "done"
+)
+
 type Task struct {
-	ID     int    `json:"id"`
-	Status string `json:"status"`
+	ID          int
+	Name        string
+	Description *string
+	Status      Status
+	CreatedAt   time.Time
 }
